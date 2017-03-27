70d1c6e3b12e223ca4775d13039acdc127fa1c35
mark anthony <captainflimflam@hotmail.com>
2017-03-27 01:52:03 +0100

    added more as too much for 1 commit

---
+++ Game/Assets/GameScripts/ButtonControl.js
@@ -0,0 +1,14 @@
+﻿function OnMouseEnter()
+{
+//change colour or the button
+GetComponent.<Renderer>().material.color = Color.white;
+}
+
+function OnMouseExit()
+{
+//change colour or the button
+
+//var aColor: Color = Vector4(1, 0.92, 0.016, 1);
+//renderer.material.color = Color.Vector4;
+GetComponent.<Renderer>().material.color = Color.blue;
+}
